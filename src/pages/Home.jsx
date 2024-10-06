18a7d03e24414a1586b9b051aa6c4d93afcff3ab
bryannrodrigues <bryan.rodrigues@yeb.com.br>
2024-10-06 08:38:36 -0300

    add:text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,111 +1,131 @@
-  import React, { useState, useEffect } from 'react'
-  import { Canvas, useFrame, useThree } from '@react-three/fiber'
-  import { OrbitControls } from '@react-three/drei'
-  import { EffectComposer, Bloom } from '@react-three/postprocessing'
-  import { Physics } from '@react-three/rapier'
-  import Scene from "../components/Scene";
-  import '../assets/styles/Home.css'
+import React, { useState, useEffect } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
+import { Physics } from "@react-three/rapier";
+import Scene from "../components/Scene";
+import "../assets/styles/Home.css";
+import { TypeAnimation } from "react-type-animation";
 
-  const FocusOnSunWithZoom = () => {
-      const { camera } = useThree();
-      const targetPosition = { x: 0, y: 20, z: 50 };  // Posição final da câmera (zoom próximo do Sol)
-      const speed = 0.05;  // Velocidade da animação (quanto menor, mais suave)
+const FocusOnSunWithZoom = () => {
+  const { camera } = useThree();
+  const targetPosition = { x: 0, y: 20, z: 50 }; // Posição final da câmera (zoom próximo do Sol)
+  const speed = 0.05; // Velocidade da animação (quanto menor, mais suave)
 
-      useFrame(() => {
-          camera.position.x += (targetPosition.x - camera.position.x) * speed;
-          camera.position.y += (targetPosition.y - camera.position.y) * speed;
-          camera.position.z += (targetPosition.z - camera.position.z) * speed;
+  useFrame(() => {
+    camera.position.x += (targetPosition.x - camera.position.x) * speed;
+    camera.position.y += (targetPosition.y - camera.position.y) * speed;
+    camera.position.z += (targetPosition.z - camera.position.z) * speed;
 
-          // Mantém a câmera olhando para o Sol (centro da cena)
-          camera.lookAt(0, 0, 0);
-      });
+    // Mantém a câmera olhando para o Sol (centro da cena)
+    camera.lookAt(0, 0, 0);
+  });
 
-      return null;
+  return null;
+};
+
+const Home = () => {
+  const [hasStarted, setHasStarted] = useState(false);
+  const [showInfoCard, setShowInfoCard] = useState(false);
+  const [canControlView, setCanControlView] = useState(false);
+  const [startZoom, setStartZoom] = useState(false); // Controla quando iniciar o zoom
+  const [overlayActive, setOverlayActive] = useState(true); // Controla a exibição do overlay
+  const [fadeOut, setFadeOut] = useState(false); // Controla a transição de fade-out
+
+  useEffect(() => {
+    const handleKeyPress = () => {
+      console.log("Tecla pressionada");
+      setHasStarted(true);
+      setShowInfoCard(true);
+      setFadeOut(true); // Inicia o fade-out do overlay
+
+      // Espera a transição terminar (4 segundos) para remover o overlay
+      setTimeout(() => {
+        setOverlayActive(false); // Remove o overlay após o fade-out completo
+      }, 4000); // Tempo da transição de clareamento suave (ajustado para 4 segundos)
+    };
+
+    if (!hasStarted) {
+      window.addEventListener("keydown", handleKeyPress);
+    }
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [hasStarted]);
+
+  const handleNextClick = () => {
+    setShowInfoCard(false);
+    setStartZoom(true); // Ativa o zoom
+    setTimeout(() => {
+      setCanControlView(true); // Habilita o controle da câmera depois do zoom
+    }, 3000); // Tempo para a animação de zoom (ajustar conforme o desejado)
   };
 
-  const Home = () => {
-      const [hasStarted, setHasStarted] = useState(false);
-      const [showInfoCard, setShowInfoCard] = useState(false);
-      const [canControlView, setCanControlView] = useState(false);
-      const [startZoom, setStartZoom] = useState(false);  // Controla quando iniciar o zoom
-      const [overlayActive, setOverlayActive] = useState(true); // Controla a exibição do overlay
-      const [fadeOut, setFadeOut] = useState(false);  // Controla a transição de fade-out
+  return (
+    <div className="app-container">
+      <Canvas camera={{ position: [0, 50, 150], far: 200000 }}>
+        <color attach="background" args={["black"]} />
+        <ambientLight intensity={0.25} />
 
-      useEffect(() => {
-          const handleKeyPress = () => {
-              console.log("Tecla pressionada");
-              setHasStarted(true);
-              setShowInfoCard(true);
-              setFadeOut(true);  // Inicia o fade-out do overlay
+        {/* Animação de zoom, quando o usuário clica em "Next" */}
+        {startZoom && <FocusOnSunWithZoom />}
 
-              // Espera a transição terminar (4 segundos) para remover o overlay
-              setTimeout(() => {
-                  setOverlayActive(false);  // Remove o overlay após o fade-out completo
-              }, 4000);  // Tempo da transição de clareamento suave (ajustado para 4 segundos)
-          };
+        {/* OrbitControls são ativados apenas quando canControlView é true */}
+        {canControlView && (
+          <OrbitControls maxDistance={450} minDistance={10} makeDefault />
+        )}
 
-          if (!hasStarted) {
-              window.addEventListener('keydown', handleKeyPress);
-          }
+        <Physics gravity={[0, 0, 0]}>
+          <Scene />
+        </Physics>
 
-          return () => {
-              window.removeEventListener('keydown', handleKeyPress);
-          };
-      }, [hasStarted]);
+        <EffectComposer>
+          <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
+        </EffectComposer>
+      </Canvas>
 
-      const handleNextClick = () => {
-          setShowInfoCard(false);
-          setStartZoom(true);  // Ativa o zoom
-          setTimeout(() => {
-              setCanControlView(true);  // Habilita o controle da câmera depois do zoom
-          }, 3000);  // Tempo para a animação de zoom (ajustar conforme o desejado)
-      };
+      {overlayActive && (
+        <div className={`overlay ${fadeOut ? "fade-out" : ""}`} />
+      )}
 
-      return (
-          <div className="app-container">
-              <Canvas camera={{ position: [0, 50, 150], far: 200000 }}>
-                  <color attach='background' args={['black']} />
-                  <ambientLight intensity={0.25} />
+      {!hasStarted && <p className="start-text">Press any key to start</p>}
 
-                  {/* Animação de zoom, quando o usuário clica em "Next" */}
-                  {startZoom && <FocusOnSunWithZoom />}
+      {showInfoCard && (
+        <div className="info-card">
+        <h2>   <TypeAnimation
+      sequence={[
+        'One', // Types 'One'
+        1000, // Waits 1s
+        'Two', // Deletes 'One' and types 'Two'
+        2000, // Waits 2s
+        'Two Three', // Types 'Three' without deleting 'Two'
+        () => {
+          console.log('Sequence completed'); // Place optional callbacks anywhere in the array
+        }
+      ]}
+      wrapper="span"
+      cursor={true}
+      repeat={Infinity}
+      style={{ fontSize: '2em', display: 'inline-block' }}
+    />
+         </h2>
+          <p>
+            Here you can explore the solar system. Use the controls to navigate
+            through space!
+          </p>
+          <button onClick={handleNextClick}>Next</button>
+        </div>
+      )}
 
-                  {/* OrbitControls são ativados apenas quando canControlView é true */}
-                  {canControlView && <OrbitControls maxDistance={450} minDistance={10} makeDefault />}
+      {canControlView && (
+        <div className="controls">
+          <button className="control-btn">Livre</button>
+          <button className="control-btn">Tour Guiada</button>
+        </div>
+      )}
+    </div>
+  );
+};
 
-                  <Physics gravity={[0, 0, 0]}>
-                      <Scene />
-                  </Physics>
-
-                  <EffectComposer>
-                      <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
-                  </EffectComposer>
-              </Canvas>
-
-              {overlayActive && (
-                  <div className={`overlay ${fadeOut ? 'fade-out' : ''}`} />
-              )}
-
-              {!hasStarted && (
-                  <p className="start-text">Press any key to start</p>
-              )}
-
-              {showInfoCard && (
-                  <div className="info-card">
-                      <h2>Welcome to the Solar System Explorer</h2>
-                      <p>Here you can explore the solar system. Use the controls to navigate through space!</p>
-                      <button onClick={handleNextClick}>Next</button>
-                  </div>
-              )}
-
-              {canControlView && (
-                  <div className="controls">
-                      <button className="control-btn">Livre</button>
-                      <button className="control-btn">Tour Guiada</button>
-                  </div>
-              )}
-          </div>
-      );
-  }
-
-  export default Home;
+export default Home;
